9319e18162fb6097fb5101a6075a6da133032d1c
joe <taojoe@gmail.com>
2017-05-30 22:42:37 +0800

    更新版本号

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.github.stuxuhai/clj-template-literal "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.github.stuxuhai/clj-template-literal "0.1.0"
+  :description "template literals from clojure"
+  :url "https://github.com/taojoe/clj-template-literal"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
